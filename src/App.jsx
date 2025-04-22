d2e4489ee554705369767a0339dab7119499652b
gustavoweb <contato@gustavoweb.com>
2025-04-22 08:57:51 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,22 @@ function App() {
   const [buscaTermo, setBuscaTermo] = useState('');
   const [pokemonDados, setPokemonDados] = useState(null);
   const [erro, setErro] = useState('');
+
+  const Busca = async () => {
+    try{
+      const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${buscaTermo.toLowerCase()}`);
+      if(!resp.ok){
+        throw new Error('Pokemon não encontrado!');
+      }
+      const dados = await resp.json();
+      setPokemonDados(dados);
+      setErro('');
+    } catch(err) {
+      setErro(err.message);
+      setPokemonDados(null);
+    }
+  };
+
   return (
     <>
       
