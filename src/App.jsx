3fea1d2ea00be096faa86969a24c46b734b5888f
gustavoweb <contato@gustavoweb.com>
2025-04-22 08:24:55 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [buscaTermo, setBuscaTermo] = useState('');
+  const [pokemonDados, setPokemonDados] = useState(null);
+  const [erro, setErro] = useState('');
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default App;
